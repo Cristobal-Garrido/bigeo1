31ba5768833e7ee929ede5606cad3b5db45f14cd
CristalFernandez <116036862+CristalFernandez@users.noreply.github.com>
2023-05-25 19:52:51 -0400

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+export { default as Formularios } from './components/Formularios';
+export { default as Tareas } from './components/Tareas';
+export { default as Correo } from './components/Correo';
+export { default as Encargados } from './components/Encargados';
+export { default as Notificaciones } from './components/Notificaciones';
+export { default as Administradores } from './components/Administradores';
+export { default as Informes } from './components/Informes';
+export { default as Calendario } from './components/Calendario';
